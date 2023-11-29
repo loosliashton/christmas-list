c02d85a968d7034febc04fe14efe0b1f418b25ff
loosliashton <loosliashton@gmail.com>
2023-11-28 21:34:37 -0700

    Moved AI call to server-side function

--- src/app/list/suggestions/suggestions.component.ts
+++ src/app/list/suggestions/suggestions.component.ts
@@ -1,15 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { List } from 'src/app/models/list';
-import OpenAI, { ClientOptions } from 'openai';
-import Configuration from 'openai';
-import { environment } from 'src/environments/environment';
-
-const config: ClientOptions = {
-  dangerouslyAllowBrowser: true,
-  apiKey: environment.openAIKey,
-  organization: 'org-sEF2fHGH9aqBtdDLRifXuEVk',
-};
+import { FirebaseService } from 'src/app/firebase.service';
 
 @Component({
   selector: 'app-suggestions',
@@ -18,48 +10,35 @@ const config: ClientOptions = {
 })
 export class SuggestionsComponent {
   list: List;
-  openai = new OpenAI(config);
   results: any[] = [];
   loading = true;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private firebase: FirebaseService
+  ) {
     this.list = data.list;
   }
 
   async ngOnInit() {
-    await this.openai.chat.completions
-      .create({
-        messages: [
-          {
-            role: 'system',
-            content:
-              "I'm putting together a gift list and need suggestions on what to add. Give me at least two suggestions and up to ten in the form of a JSON array in an object called 'suggestions'. Please return the following JSON for each suggestion: {name: string, description: string}.",
-          },
-          {
-            role: 'user',
-            content: this.generateContentString(this.list),
-          },
-        ],
-        model: 'gpt-3.5-turbo-1106',
-        response_format: { type: 'json_object' },
-        temperature: 0.2,
-      })
-      .then((res) => {
-        this.processResults(res);
+    this.getSuggestions();
+  }
+
+  getSuggestions() {
+    this.firebase.getSuggestions(this.list).then((res) => {
+      this.processResults(res);
+    });
+  }
+
+  processResults(res: any) {
+    if (!res.choices || !res.choices[0]) {
+      this.results.push({
+        name: 'No suggestions found',
+        description: 'Try adding more items to your list',
       });
-  }
-
-  generateContentString(list: List): string {
-    let content = 'Here is my current list of items. Please give me at least two suggestions (preferably more) to add to the list. \n';
-    if (!list.items) return content;
-    for (let item of list.items) {
-      content += `Item: ${item.name}; `;
-      content += `URL: ${item.url}\n`;
+      this.loading = false;
+      return;
     }
-    return content;
-  }
-
-  processResults(res: OpenAI.Chat.Completions.ChatCompletion) {
     let result = res.choices[0].message.content;
     if (!result) return;
     let suggestions = JSON.parse(result).suggestions;

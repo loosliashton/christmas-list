75af62693da01157588502ecd426bdca48b6e42d
Ashton Loosli <loosliashton@gmail.com>
2024-11-19 16:48:24 -0500

    Working on asin handling

--- src/app/list/item/item.component.ts
+++ src/app/list/item/item.component.ts
@@ -1,18 +1,20 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Item } from 'src/app/models/item';
 import { FirebaseService } from 'src/app/firebase.service';
+import * as amazonAsin from 'amazon-asin';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrl: './item.component.css',
+  styleUrls: ['./item.component.css'],
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit {
   item: Item;
   listId: string;
   spoilers: boolean;
+  camelUrl: string | null = null;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -21,6 +23,10 @@ export class ItemComponent {
     this.item = data.item;
     this.listId = data.listId;
     this.spoilers = data.spoilers;
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.camelUrl = await this.getCamelUrlIfAmazon();
   }
 
   togglePurchase() {
@@ -32,13 +38,24 @@ export class ItemComponent {
   }
 
   isAmazonUrl(): boolean {
-    const amazonUrlPattern = /^https:\/\/www\.amazon\.com\/.*\/dp\/.+/;
+    const amazonUrlPattern =
+      /^https?:\/\/(www\.)?amazon\.com\/dp\/[A-Z0-9]{10}\/?\?.*$/;
     return this.item.url ? amazonUrlPattern.test(this.item.url) : false;
   }
 
   getCamelUrl(): string {
     // Get the item ID from the Amazon URL
+    var amazonAsin = require('amazon-asin');
+
+    amazonAsin.asyncParseAsin("http://amzn.to/2eEPcFk");
     const itemId = this.item.url.split('/dp/')[1].split('/')[0];
     return `https://camelcamelcamel.com/product/${itemId}`;
   }
+
+  async getCamelUrlIfAmazon(): Promise<string | null> {
+    var amazonAsin = require('amazon-asin');
+    let result = await amazonAsin.asyncParseAsin(this.item.url);
+    return null;
+  }
 }
+

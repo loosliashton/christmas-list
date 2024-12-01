3b9f1fa5151bdf8f8b79015f2495d23ea0a84007
loosliashton <loosliashton@gmail.com>
2024-11-30 19:14:30 -0500

    Saved the last used email to make logging in easier

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,20 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   badEmail: boolean = false;
   email: string = '';
-  christmasTheme: boolean = true;
+  christmasTheme: boolean = false;
 
   ngOnInit(): void {
     const currentDate = new Date();
     const currentMonth = currentDate.getMonth() + 1;
-    const currentDay = currentDate.getDate();
 
-    if (currentMonth === 12 && currentDay >= 1 && currentDay <= 25) {
+    if ([11, 12].includes(currentMonth)) {
       this.christmasTheme = true;
+    }
+
+    // Retrieve the last used email
+    const lastEmail = localStorage.getItem('lastEmail');
+    if (lastEmail) {
+      this.email = lastEmail;
     }
   }
 
@@ -29,6 +34,7 @@ export class HomeComponent implements OnInit {
     email = email.toLowerCase().trim();
 
     if (emailPattern.test(email)) {
+      localStorage.setItem('lastEmail', email);
       this.router.navigate(['/my-lists'], { queryParams: { email } });
     } else {
       this.badEmail = true;
